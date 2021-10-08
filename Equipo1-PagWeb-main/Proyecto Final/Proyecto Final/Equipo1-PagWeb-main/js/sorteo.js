623dbedc67146a352a494fa00213caa80ba73ef6
Linosolis23 <lino.ma41393@cesurformacion.com>
2021-10-08 09:32:50 +0200

    Comentando codigo

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/sorteo.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/sorteo.js
@@ -1,11 +1,11 @@
 
 function sorteo() {
-    var numero = document.getElementById("sorteo-input").value;
-    var premio = Math.floor(Math.random() * 10) + 1; //número premiado
+    var numero = document.getElementById("sorteo-input").value;//el numero escrito en el formulario entra aquí
+    var premio = Math.floor(Math.random() * 10) + 1; //el número se genera random cada vez que se ejecuta la función
     var ganador = false;
     
 
-    if (isNaN(numero)) {
+    if (isNaN(numero)) { //Lector para ver si escribes letras en los numeros, si lo pones saltará el mensaje
         document.getElementById("texto-resultado").innerText = "No se permiten letras";
     } else {
         if (numero == premio) {
@@ -18,7 +18,7 @@ function sorteo() {
 }
 
 
-function mostrarMensajeSorteo(ganador) {
+function mostrarMensajeSorteo(ganador) { //Funcion muestra un mensaje si el numero que pones es igual al ganador
     if (ganador) {
         document.getElementById("texto-resultado").innerText = "Enhorabuena";
 
