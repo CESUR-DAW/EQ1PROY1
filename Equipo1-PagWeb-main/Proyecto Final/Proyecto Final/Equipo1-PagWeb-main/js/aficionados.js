f1164a696f1a90b76675fb813a95c12082d7c807
Daniel Castillo Ruiz <danicaru96@gmail.com>
2021-10-11 13:57:05 +0200

    Actualizacion Punto 2

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/aficionados.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/aficionados.js
@@ -7,15 +7,24 @@ function bienvenidoAficionado() {
     mostrarTipoAficionado(calculoAños);
 
 }
-function mostrarMensaje(nombre, calculoAños) {
-    if (calculoAños >= 0) {
-        document.getElementById('mensajeBienvenida').innerHTML = "Bienvenid@ a la pagina Aficionados Usuario: " + nombre + " usted lleva en el club: " + calculoAños + " años";
-    }else{
-        document.getElementById('mensajeBienvenida').innerHTML = "La fecha introducida no es valida";
+
+function mostrarMensaje(nombre, calculoAños) { // Mostramos el mensaje de bienvenida
+    if (calculoAños >= 0 && nombre.length > 0) { // Descartamos fecha no valdad o un nombre vacio 
+        if (window.navigator.language === 'es-ES') { // Comparamos el idioma del navegador de cada usario.
+            document.getElementById('mensajeBienvenida').innerHTML = "Bienvenid@ a la pagina Aficionados Usuario: " + nombre + " usted lleva en el club: " + calculoAños + " años";
+        } else if (window.navigator.language === 'fr') {
+            document.getElementById('mensajeBienvenida').innerHTML = "Bienvenue sur la page Fans d'utilisateurs : " + nombre + " vous portez dans le club: " + calculoAños + " années";
+
+        } else {
+            document.getElementById('mensajeBienvenida').innerHTML = "Welcome to the page Fans User: " + nombre + " you've been in the club for: " + calculoAños + " years";
+
+        }
+    } else {
+        document.getElementById('mensajeBienvenida').innerHTML = "Los datos introducidos no son correctos";
     }
 }
 
-function mostrarTipoAficionado(year) {
+function mostrarTipoAficionado(year) { //Mensaje del tipo aficionado
     if (year >= 0 && year <= 2) {
         document.getElementById('tipoAficionado').innerHTML = "Usted es un aficionado de tipo básico";
     } else if (year > 2 && year <= 5) {
@@ -28,10 +37,10 @@ function mostrarTipoAficionado(year) {
 }
 
 function calcularAños(fechaIntroducida) { // nos calcula los años
-    const FechaHoy = Date.now(); 
+    const FechaHoy = Date.now();
     const hoy = new Date(FechaHoy);
     const msAños = 31557600000; // milisegundos que tiene un año
-    nuevaFecha = fechaIntroducida; 
+    nuevaFecha = fechaIntroducida;
     calculo = hoy - nuevaFecha; // nos calcula los milisegundos entre la fecha indtroducida y hoy
     calculo = Math.trunc(calculo / msAños); // nos calcula los años y nos trunca al valor entero
     return calculo;
