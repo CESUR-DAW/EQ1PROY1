abeffb650f5e9509a3baf7e55506d0b96c54cc8e
Daniel Castillo Ruiz <danicaru96@gmail.com>
2021-10-14 23:09:11 +0200

    nuevo fichero js para areaprivada
    - division ficheros js para las areas privadas

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
@@ -1,46 +1,5 @@
-
-var usuario = "dani";
-var contraseña = "pass1";
-var intentos = 3;
 var arrayJug = [];
 var borrarEle;
-
-//Esta funcion recoje el usuario y la contraseña de los inputs y las compara con las correctas
-function login() {
-    var passInput = document.getElementById('pass').value;
-    var userInput = document.getElementById('user').value;
-    var loginValido = false;
-
-    //comprobamos que tanto el usuario como el password han sido introducidos
-    if(passInput != '' && userInput != '') {
-        //Si la contraseña es correcta la variable cambia a true permitiendonos el acceso
-        if(contraseña == passInput && usuario == userInput) {
-            loginValido = true;
-        //Si la contraseña no es correcta se resta un intento y se muestra un mensaje indicandolo
-        }else {
-            if(intentos > 1) {
-                intentos--;
-                document.getElementById('mensajeLogin').innerHTML = "Contraseña erronea, siga intentandolo, quedan "+intentos+" intentos";
-                document.getElementById('mensajeLogin').style.display = "block";
-                document.getElementById('pass').value = '';
-                document.getElementById('user').value = '';
-            
-            //Al agotar los 3 intentos nos redirige a la pagina HOME
-            }else {
-                window.location.href = "index.html";
-            }
-        }
-
-        //Por ultimo se comprueba que el login es valido y se redirige a la pagina deseada
-        if(loginValido == true) {
-            window.location.href = "areaPrivada.html";
-        }
-    //si falta algun campo por rellenar manda un mensaje de aviso
-    }else {
-        document.getElementById('mensajeLogin').innerHTML = "Por favor rellene todos los campos";
-        document.getElementById('mensajeLogin').style.display = "block";
-    }
-}
 
 //con esta funcion añadimos al jugador que introducimos en el input y lo almacenamos en un array
 function añadirJugador() {
@@ -67,6 +26,7 @@ function añadirJugador() {
 
 //esta funcion borra al jugador que coincide con lo introducido en el input
 function borrarJugador() {
+
     var jugador = document.getElementById('jugador').value;
     var lista = "";
 
@@ -90,4 +50,3 @@ function borrarJugador() {
 
     
 }
-
