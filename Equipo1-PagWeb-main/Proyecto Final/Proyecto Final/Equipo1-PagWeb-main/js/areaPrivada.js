bd24612aba7a0f64823046716cf2d06576b3b8a2
Daniel Martínez <martinezarrabal85@gmail.com>
2021-10-22 10:35:55 +0200

    formulario areaprivada

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
@@ -1,42 +1,91 @@
-var listaJugadores = [];
+// var listaJugadores = [];
 
 
-//con esta funcion añadimos al jugador que introducimos en el input y lo almacenamos en un array
-function añadirJugador() {
-    var jugador = document.getElementById('jugador').value;
-    if(jugador != "" && jugador != "Introduce Jugador") {
+
+// //con esta funcion añadimos al jugador que introducimos en el input y lo almacenamos en un array
+// function añadirJugador() {
+//     var jugador = document.getElementById('jugador').value;
+//     if(jugador != "" && jugador != "Introduce Jugador") {
         
-        listaJugadores.push(jugador); //añadimos jugadores
+//         listaJugadores.push(jugador); //añadimos jugadores
 
-        document.getElementById('jugador').value = ""; //volvemos a vaciar el input
-        mostrarLista();
-        return listaJugadores;
-    }else{
-        document.getElementById('jugador').value = "Introduce Jugador"; 
+//         document.getElementById('jugador').value = ""; //volvemos a vaciar el input
+//         mostrarLista();
+//         return listaJugadores;
+//     }else{
+//         document.getElementById('jugador').value = "Introduce Jugador"; 
+//     }
+// }
+
+// //esta funcion borra al jugador que coincide con lo introducido en el input
+// function borrarJugador() {
+
+//     var jugador = document.getElementById('jugador').value;
+//     var indice = listaJugadores.indexOf(jugador);
+
+//     if (indice > -1) { // Vemos que funciona bien por que si es -1 significa que no lo encuentra
+//         listaJugadores.splice(indice, 1); // borramos el jugar del array
+
+//     }
+//     document.getElementById('jugador').value = ""; //volvemos a vaciar el input
+//     mostrarLista();
+//     return listaJugadores;
+// }
+
+// function mostrarLista() {
+//     var lista = ""
+//     listaJugadores.forEach(jugador => {
+//         lista +="<li id='"+jugador+"'>"+jugador+"</li>"; 
+//     });
+//     document.getElementById('jugLista').innerHTML = lista;
+// }
+
+var array = [];
+
+class entrada {
+    constructor(titulo, fecha, hora, lugar, precio, numEntradas) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.precio = precio;
+        this.numEntradas= "";
     }
 }
 
-//esta funcion borra al jugador que coincide con lo introducido en el input
-function borrarJugador() {
-
-    var jugador = document.getElementById('jugador').value;
-    var indice = listaJugadores.indexOf(jugador);
-
-    if (indice > -1) { // Vemos que funciona bien por que si es -1 significa que no lo encuentra
-        listaJugadores.splice(indice, 1); // borramos el jugar del array
-
+function validarDatos() {
+    var datos = document.getElementsByClassName("dato");
+    var valido = true;
+    for(i=0; i<=datos.length; i++) {
+        datos[i].style.backgroundColor = "white";
+        datos[i].style.border = "2px solid black";
+        if(datos[i].value == "") {
+            datos[i].style.backgroundColor = "#F6CECE";
+            datos[i].style.border = "3px solid red";
+            valido = false;
+        }
     }
-    document.getElementById('jugador').value = ""; //volvemos a vaciar el input
-    mostrarLista();
-    return listaJugadores;
+    if(valido == true) {
+        return true;
+    }else {
+        return false;
+    }
 }
 
-function mostrarLista() {
-    var lista = ""
-    listaJugadores.forEach(jugador => {
-        lista +="<li id='"+jugador+"'>"+jugador+"</li>"; 
-    });
-    document.getElementById('jugLista').innerHTML = lista;
-    
-    
+function crearEntrada() {
+
+    var titulo = document.getElementById("titulo").value;
+    var fecha = document.getElementById("fecha").value;
+    var hora = document.getElementById("hora").value;
+    var lugar = document.getElementById("lugar").value;
+    var precio = document.getElementById("precio").value;
+    var numEntradas = document.getElementById("numEntradas").value;
+
+    const datos_entrada = new entrada(titulo, fecha, hora, lugar, precio, numEntradas);
+
+    array.push(datos_entrada);
+
+    alert(array[0].lugar);
+  
 }
+
