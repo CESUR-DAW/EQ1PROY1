00f4bdc2d02881c92965f2f34d6b014646326994
Daniel Martínez <martinezarrabal85@gmail.com>
2021-10-27 20:36:15 +0200

    cambios pintarEntradas

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/areaPrivada.js
@@ -6,7 +6,7 @@
 // function añadirJugador() {
 //     var jugador = document.getElementById('jugador').value;
 //     if(jugador != "" && jugador != "Introduce Jugador") {
-
+        
 //         listaJugadores.push(jugador); //añadimos jugadores
 
 //         document.getElementById('jugador').value = ""; //volvemos a vaciar el input
@@ -41,6 +41,7 @@
 // }
 
 var array = [];
+var contador;
 
 class entrada {
     constructor(titulo, fecha, hora, lugar, precio, numEntradas) {
@@ -49,29 +50,30 @@ class entrada {
         this.hora = hora;
         this.lugar = lugar;
         this.precio = precio;
-        this.numEntradas = numEntradas;
+        this.numEntradas = "";
+        this.ident = Math.floor(Math.random() * 9999)+1000;
     }
 }
 
 function validarDatos() {
     var datos = document.getElementsByClassName("dato");
     var valido = true;
-    for (i = 0; i <= datos.length; i++) {
+    for(i=0; i<=datos.length; i++) {
         datos[i].style.backgroundColor = "white";
         datos[i].style.border = "2px solid black";
-        if (datos[i].value == "") {
+        if(datos[i].value == "") {
             datos[i].style.backgroundColor = "#F6CECE";
             datos[i].style.border = "3px solid red";
             valido = false;
         }
     }
-    if (valido == true) {
+    if(valido == true) {
         return true;
-    } else {
+    }else {
         return false;
     }
 }
- 
+
 function crearEntrada() {
     var titulo = document.getElementById("titulo").value;
     var fecha = document.getElementById("fecha").value;
@@ -79,6 +81,7 @@ function crearEntrada() {
     var lugar = document.getElementById("lugar").value;
     var precio = document.getElementById("precio").value;
     var numEntradas = document.getElementById("numEntradas").value;
+    var ident = Math.floor(Math.random() * 9999)+1000;
 
     const datos_entrada = new entrada(titulo, fecha, hora, lugar, precio, numEntradas);
 
@@ -88,12 +91,12 @@ function crearEntrada() {
 //esta funcion guarda la array con todos los objetos entrada en localStorage y la manda pintar
 function guardarEntrada(datos) {
     array.push(datos);
-    if (array.length == 0) {
+    if(array.length == 0) {
         localStorage.setItem("entradas", JSON.stringify(array[0]));
-    } else {
-        localStorage.clear();
-        localStorage.setItem("entradas", JSON.stringify(array));
-        pintarEntradas(datos);
+    }else {
+        localStorage.clear();//limpiamos el localStorage
+        localStorage.setItem("entradas", JSON.stringify(array));//guardamos la nueva array en localStorage
+        pintarEntradas();
     }
 }
 
@@ -101,30 +104,30 @@ function guardarEntrada(datos) {
 function obtenerEntrada() {
     var guardado = localStorage.getItem("entradas");
     var json = JSON.parse(guardado);
-    if (json != null) {
-        for (i = 0; i <= json.length - 1; i++) {
+    if(json != null) {
+        array = [];
+        for(i=0; i<=json.length-1; i++) {
             array.push(json[i]);
-            pintarEntradas(json[i]);
         }
+        contador = array.length;
     }
 }
 
 //pinta las entradas que se le van pasando
-function pintarEntradas(entradas) {
-    var prueba = document.createElement("div");
-    prueba.setAttribute("class", "entradas");
-    prueba.innerHTML = "<div class='entrada'><p>" + entradas.titulo + "</p><p>dia: " + entradas.fecha + "</p><p>Hora: " + entradas.hora + "</p></div>";
-    var contenedor = document.getElementById("galeria");
-    contenedor.appendChild(prueba);
+function pintarEntradas() {
+    obtenerEntrada();
+    document.getElementById("galeria").innerHTML = "";
+    for(i=0; i<=array.length-1; i++) {
+        var prueba = document.createElement("div");
+        prueba.setAttribute("class", "entradas");
+        prueba.innerHTML = "<div id='"+array[i].ident+"' class='entrada'><p>"+array[i].titulo+"</p><p>dia: "+array[i].fecha+"</p><p>Hora: "+array[i].hora+"</p><p>Id: "+array[i].ident+"</p></div>";
+        var contenedor = document.getElementById("galeria");
+        contenedor.appendChild(prueba);
+    }   
 }
-
 
 function eliminarEntrada() {
     document.getElementById("entrada").style.display = "none";
 }
 
-function mostrarEntradas() {
-    var entradas = localStorage.getItem("entradas");
-    document.getElementsByName("resumenEntradas").innerHTML = pintarEntradas(entradas)
-}
 
