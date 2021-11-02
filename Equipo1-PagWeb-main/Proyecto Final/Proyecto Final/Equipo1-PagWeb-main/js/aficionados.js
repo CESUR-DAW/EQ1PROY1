e9e0306c6fe1de3bca729842c24ab314d77ecef7
Daniel Castillo Ruiz <danicaru96@gmail.com>
2021-11-02 10:54:00 +0100

    mensaje en div resumenEntradas

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/aficionados.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/aficionados.js
@@ -156,5 +156,5 @@ function actualizarEntradas(idEntrada,entradasAComprar) {
     guardarEntrada(listaEntradas[idEntrada]);
     console.log(listaEntradas);
     pintarEntradas();
-    
+    document.getElementById('resumenEntradas').innerHTML="Adri se la chupa a Lino.Compra realizada con éxito/Gracias por su compra";
 }
