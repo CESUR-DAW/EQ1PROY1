b611952efdfaed188038e1b39373d1dbdb652d73
adrik1402 <adrikalvani@hotmail.com>
2021-12-14 20:12:08 +0100

    story 2 terminada

--- Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/comentarios.js
+++ Equipo1-PagWeb-main/Proyecto Final/Proyecto Final/Equipo1-PagWeb-main/js/comentarios.js
@@ -5,26 +5,37 @@ usuarios().then(arrayUsuarios => {
     mostrarUsuarios(arrayUsuarios);
 });
 
-
+// Imprimir Comentarios 
+function comentarios() {
+    usuarios().then(arrayUsuarios => {
+        mostrarComentarios().then(arrayPost => {
+            mostrarTodo(arrayPost, arrayUsuarios);
+        })
+    });
+}
 //Guardamos los Usuarios en un Array
 async function usuarios() {
+    var arrayUsuarios;
     var arrayUsuarios = await fetch("https://jsonplaceholder.typicode.com/users")
         .then((response) => {
             return response.json();
         })
         //Guardamos la respuesta en un array
-        .then((json) => {
-            return json;
+        .then((respuesta) => {
+            return respuesta;
         })
         .catch((error) => {
-            console.log(error);
+            console.log("Hubo un problema con la petición Fetch" + error);
         })
-    //Devolvemos el array para trabajar con este
+        //Devolvemos el array para trabajar con este
     return arrayUsuarios;
 }
 
+
+
 function mostrarUsuarios(arrayUsuarios) {
-    for (let i = 0; i < arrayUsuarios.length; i++) {
+    console.log(arrayUsuarios);
+    for (i = 0; i < arrayUsuarios.length; i++) {
         document.getElementById("idUser").innerHTML += `<option value ='${arrayUsuarios[i].id}'>
         ${arrayUsuarios[i].id}: ${arrayUsuarios[i].name}</option>`
 
@@ -33,13 +44,50 @@ function mostrarUsuarios(arrayUsuarios) {
 
 // //Story 2
 
-// Imprimir Comentarios 
-function comentarios() {
-    mostrarUsuarios().then(arrayUsuarios =>{
-        mostrarComentarios().then(arrayComentarios =>{
-            mostrarComentarios(arrayComentarios,arrayUsuarios);
+//Guardamos los comentarios en un Array
+async function mostrarComentarios() {
+    // Llamamos a la api con fetch (crea una promesa)
+    var arrayPost;
+    // Promesa, recogemos la respuesta con them
+    var arrayPost = await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+            return response.json();
         })
-    });
+        // guardamos la respuesta .data en un array
+        .then((respuesta) => {
+
+            return respuesta;
+
+        })
+        .catch((error) => {
+            console.log("Hubo un problema con la petición Fetch" + error);
+        });
+    return arrayPost;
+}
+
+
+function mostrarTodo(arrayPost, arrayUsuarios) {
+    //contador de comentarios
+    contador = 1;
+
+    //con este  for recorremos los  usuarios imprimiendo el usuario  y el nombre
+    for (i = 0; i < arrayUsuarios.length; i++) {
+
+        document.getElementById("comentarios").innerHTML +=
+            ` <hr> <h2 class="usuarioTitulo">${arrayUsuarios[i].name}. Id de usuario  ${arrayUsuarios[i].id}</h2> `
+            // este  for recorre el post  y comprueba si el comentario coincide con el id del usuario 
+        for (x = 0; x < arrayPost.length; x++) {
+            //imprimimos todos sus titulos y comentarios
+            if (arrayPost[x].userId == arrayUsuarios[i].id) {
+                document.getElementById("comentarios").innerHTML += ` <h3>Comentario ${contador} : ${arrayPost[x].title}</h3>
+                <p>El contenido del comentario ${contador}  es :  ${arrayPost[x].body}</p> `
+                    //aumentamos la variable por cada  comentario
+                contador++;
+            }
+        }
+        //reiniciamos el contador
+        contador = 1;
+    }
 }
 
 
@@ -48,31 +96,7 @@ function comentarios() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//NO SE SABE SUS FUNCIONES
 
 function select() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -92,16 +116,16 @@ function select() {
 
 function exportadatos() {
     fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: document.getElementById("titulo").value,
-            body: document.getElementById("comentario").value,
-            /*userId: document.getElementById("idusu").value,*/
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
+            method: 'POST',
+            body: JSON.stringify({
+                title: document.getElementById("titulo").value,
+                body: document.getElementById("comentario").value,
+                /*userId: document.getElementById("idusu").value,*/
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
         .then((response) => response.json())
         .then((json) => console.log(json));
 }
